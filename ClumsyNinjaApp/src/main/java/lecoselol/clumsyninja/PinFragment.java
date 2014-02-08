1bd948dff4e9af44f5064549840cd098cea00cdf
Sebastiano Poggi <poggos@gmail.com>
2014-02-08 16:53:17 +0100

    MOAR fixes, plus stuff.

--- ClumsyNinjaApp/src/main/java/lecoselol/clumsyninja/PinFragment.java
+++ ClumsyNinjaApp/src/main/java/lecoselol/clumsyninja/PinFragment.java
@@ -6,19 +6,19 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
- * Created by rock3r on 08/02/2014.
+ * You're supposed to input your PIN in this thing.
  */
 public class PinFragment extends Fragment {
 
     private TextView mPinText;
+    private OnPinChangedListener mListener;
+    private String mPin = "";
 
-    private String mPin;
-
-    public PinFragment() {
+    public void setListener(OnPinChangedListener listener) {
+        mListener = listener;
     }
 
     @Override
@@ -28,16 +28,19 @@ public class PinFragment extends Fragment {
         PinPadListener l = new PinPadListener();
 
         // I don't remember the fucking view IDs LOL
-        ViewGroup rootGroup = (ViewGroup) rootView;
-        int count = rootGroup.getChildCount();
-        for (int i = 0; i < count; i++) {
-            View v = rootGroup.getChildAt(i);
-            if (v instanceof Button) {
-                v.setOnClickListener(l);
-            }
-        }
+        assert rootView != null;     // Debug shit here!
+        rootView.findViewById(R.id.txt_one).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_due).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_troix).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_vier).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_cinco).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_kuusi).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_zeven).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_oito).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_dziewięć).setOnClickListener(l);
+        rootView.findViewById(R.id.txt_null).setOnClickListener(l);
 
-        mPinText = (TextView) rootGroup.findViewById(R.id.txt_pin);
+        mPinText = (TextView) rootView.findViewById(R.id.txt_pin);
         mPinText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,12 +54,20 @@ public class PinFragment extends Fragment {
     public void addNumber(char num) {
         mPin = mPin + num;
         mPinText.setText(mPin);
+
+        if (mListener != null) {
+            mListener.onPinChanged(mPin);
+        }
     }
 
     public void removeNumber() {
         if (!TextUtils.isEmpty(mPin)) {
             mPin = mPin.substring(0, mPin.length() - 1);
             mPinText.setText(mPin);
+
+            if (mListener != null) {
+                mListener.onPinChanged(mPin);
+            }
         }
     }
 
@@ -72,26 +83,44 @@ public class PinFragment extends Fragment {
                 // WTF now I'm hating my smartassness
                 case R.id.txt_one:
                     addNumber('1');
+                    break;
                 case R.id.txt_due:
                     addNumber('2');
+                    break;
                 case R.id.txt_troix:
                     addNumber('3');
+                    break;
                 case R.id.txt_vier:
                     addNumber('4');
+                    break;
                 case R.id.txt_cinco:
                     addNumber('5');
+                    break;
                 case R.id.txt_kuusi:
                     addNumber('6');
+                    break;
                 case R.id.txt_zeven:
                     addNumber('7');
+                    break;
                 case R.id.txt_oito:
                     addNumber('8');
+                    break;
                 case R.id.txt_dziewięć:
                     addNumber('9');
+                    break;
                 case R.id.txt_null:
                     addNumber('0');
+                    break;
+                default:
+                    addNumber('?');
             }
         }
     }
 
+    /**
+     * Listener for PIN changes.
+     */
+    public interface OnPinChangedListener {
+        public void onPinChanged(String currentPin);
+    }
 }
