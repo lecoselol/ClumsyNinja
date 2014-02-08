ffe9e1df6baac187c59b3446f040934e00183e08
Sebastiano Poggi <poggos@gmail.com>
2014-02-08 23:25:15 +0100

    It's dangerous out there, take this

--- ClumsyNinjaApp/src/main/java/lecoselol/clumsyninja/NoteDetailFragment.java
+++ ClumsyNinjaApp/src/main/java/lecoselol/clumsyninja/NoteDetailFragment.java
@@ -1,12 +1,11 @@
 package lecoselol.clumsyninja;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import lecoselol.clumsyninja.dummy.DummyContent;
 
 /**
